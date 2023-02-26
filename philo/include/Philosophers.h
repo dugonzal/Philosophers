00b29957a80665519728f8171e5de76ab10ed8e6
dugonzal <ciclocontinuo2@gmail.com>
2023-02-26 10:52:25 +0000

    asd

---
+++ philo/include/Philosophers.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/24 23:15:39 by ciclo             #+#    #+#             */
+/*   Updated: 2023/02/24 23:36:11 by ciclo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOFERS
+#define PHILOSOFERS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_phlo
+{
+	/* data */
+}t_philo;
+
+
+#endif
