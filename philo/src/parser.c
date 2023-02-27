10cd6da39ae0056a33d3a81e99773a59eaf37271
dugonzal <ciclocontinuo2@gmail.com>
2023-02-27 18:14:06 +0000

    agrego mensaje de ayuda con las opciones --help y resumida -h en español

--- philo/src/parser.c
+++ philo/src/parser.c
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 14:26:50 by ciclo             #+#    #+#             */
-/*   Updated: 2023/02/27 18:07:01 by ciclo            ###   ########.fr       */
+/*   Updated: 2023/02/27 18:13:20 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 void	help_msg(void)
 {
-	printf (BLUE"Philosophers: opcion help -> flag: [--help] [-h]\n"RESET);
+	printf (BLUE"Philosophers: opcion help -> flag: [--help] [-h]\n\n"RESET);
 	printf ("Usage:	 ./philosofers number_of_philosophers time_to_die time_to_eat \
 	time_to_sleep [number_of_times_each_philosopher_must_eat]\n");
-	printf ("Example: ./philosofers 5 800 200 200\n");
+	printf ("Uso:	 ./filosofers numero_de_filosofos tiempo_para_morir \
+	tiempo_para_comer tiempo_para_dormir [numero_de_veces_que_cada_filosofo_debe_comer]\n");
+	printf ("Example: ./philosofers 5 800 200 200 [5]\n");
 	exit (EXIT_SUCCESS);
 }
 
@@ -35,6 +37,6 @@ void	parser(int ac, char **av, t_data *data)
 		data->must_eat = ft_atoi(av[5]);
 	data->philo = malloc(sizeof(t_philo) * data->philo_num);
 	if (!data->philo)
-		error("Error: malloc failed\n");
+		error(RED"Error: malloc failed\n"RESET);
 	memset(data->philo, 0, sizeof(t_philo) * data->philo_num);
 }
