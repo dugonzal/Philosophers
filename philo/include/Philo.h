fe9848f9a53daba8f6991723be822f67bddb4031
dugonzal <ciclocontinuo2@gmail.com>
2023-03-30 14:22:52 +0000

    actualizo .h para norminette

--- philo/include/Philo.h
+++ philo/include/Philo.h
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 23:15:39 by ciclo             #+#    #+#             */
-/*   Updated: 2023/03/30 14:08:38 by ciclo            ###   ########.fr       */
+/*   Updated: 2023/03/30 14:22:20 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,35 +31,38 @@
 
 typedef struct s_data
 {
-	int  philo_num;	// numero de filosofos
-	int  time_to_die; // tiempo que muere si no come;
-	int  time_to_eat; // tiempo que come
-	int			time_to_sleep; // tiempo que duerme
-  int     time_to_thinking; // tiempo que piensa
-	int			must_eat; // -1 si no se pasa como argumento
-  int     dead; // aqui guradamos si hay algun filsf muerto
-  long int     time;
-  pthread_t	*thread; // hilo del filosofo
+	int 			philo_num;	// numero de filosofos
+	int 			time_to_die; // tiempo que muere si no come;
+	int 			time_to_eat; // tiempo que come
+	int				time_to_sleep; // tiempo que duerme
+	int				time_to_thinking; // tiempo que piensa
+	int				must_eat; // -1 si no se pasa como argumento
+	int				dead; // aqui guradamos si hay algun filsf muerto
+	long int		time;
+	pthread_t		*thread; // hilo del filosofo
 	pthread_mutex_t	*forks; // array de mutex de los tenedores
-  }	t_data;
+}	t_data;
 
 typedef struct s_philo
 {
-	int			id; // id del filosofo
-	int			left_fork; // id del (tenedor o mutex) izquierdo
-	int			right_fork; // id del (tenedor o mutex) derecho
-	int			eat_count; // numero de veces que ha comido
-  long int time;
-  struct s_data *data; // estructura general del programa
+	int				id; // id del filosofo
+	int				left_fork; // id del (tenedor o mutex) izquierdo
+	int				right_fork; // id del (tenedor o mutex) derecho
+	int				eat_count; // numero de veces que ha comido
+	long int		time;
+	struct s_data 	data; // estructura general del programa
 }t_philo;
 
-int		ft_atoi(const char *str, t_data *data);
-t_data 	*parser(int ac, char **av, t_data *data);
-void	error(char *s);
-int		ft_strlen(char *str);
-int		ft_strcmp(char *s1, char *s2);
-void  init_threads(t_data *data);
-long long get_time(void);
-void clean(t_data *data, t_philo *philo);
+int			ft_atoi(const char *str, t_data *data);
+t_data		*parser(int ac, char **av, t_data *data);
+void		error(char *s);
+int			ft_strlen(char *str);
+int			ft_strcmp(char *s1, char *s2);
+void 		init_threads(t_data *data);
+long int	get_time(void);
+void		clean(t_data *data, t_philo *philo);
+long int	time_diff(long int present, long int past);
+void		time_time(long int time);
+void		print(char *str, t_philo *philo, t_data *data);
 
 #endif
